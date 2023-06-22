e5117e3db3c9147b069e2ce7a1b28e7f26b38d25
Nataliia Kulieshova <nataliia.kulieshova@gmail.com>
2023-06-21 18:22:24 -0600

    alert v2.0
    Added alert API and notification system to 16to9.php and 9to16.php
    Edited html templates to display warnings
    Fixed minor bugs

--- 9to16.php
+++ 9to16.php
@@ -6,7 +6,9 @@
     <link rel='preconnect' href='https://fonts.googleapis.com'>
     <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
     <link href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap' rel='stylesheet'>
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js'></script>
     <title>Weatherbug forecast</title>
+
     <style>
         body {
             padding: 0;
@@ -69,6 +71,39 @@
         #forecast_4 {
             right: 0vw;
         }
+
+        #alert {
+            width: 84vw;
+            height: auto;
+            background-color: white;
+            color: red;
+            font-family: 'Inter', sans-serif;
+            border-radius: 30px;
+            position: absolute;
+            right: 3vw;
+            font-size: 2.3vw;
+            padding: 1vw 3vw 1vw 6vw;
+        }
+
+        #alert_icon {
+            width: 3vw;
+            position: relative;
+            margin-left: -4.6vw;
+        }
+
+        #alert {
+            display: flex;
+            align-items: center;
+        }
+
+        #alert_icon {
+            margin-right: 10px;
+            /* Adjust as needed */
+        }
+
+        #alert span {
+            flex: 1;
+        }
     </style>
 </head>
 
@@ -87,59 +122,59 @@
     // reads weathercode and displays according icon and description
     if ($weather_now == 0) {
         $picture = "sunny";
-        $bg_image = "../backgrounds/sunny.jpg";
+        $bg_image = "../assets/backgrounds/sunny.jpg";
         $weather_decr = "Clear sky";
     } else if ($weather_now == 1) {
         $picture = "partly_cloudy";
-        $bg_image = "../backgrounds/sunny.jpg";
+        $bg_image = "../assets/backgrounds/sunny.jpg";
         $weather_decr = "Mainly clear";
     } else if ($weather_now == 2) {
         $picture = "partly_clear";
-        $bg_image = "../backgrounds/cloudy.jpg";
+        $bg_image = "../assets/backgrounds/cloudy.jpg";
         $weather_decr = "Partly cloudy";
     } else if ($weather_now == 3) {
         $picture = "cloudy";
-        $bg_image = "../backgrounds/cloudy.jpg";
+        $bg_image = "../assets/backgrounds/cloudy.jpg";
         $weather_decr = "Overcast";
     } else if ($weather_now == 45 || $weather_now == 48) {
         $picture = "fog";
-        $bg_image = "../backgrounds/cloudy.jpg";
+        $bg_image = "../assets/backgrounds/cloudy.jpg";
         $weather_decr = "Foggy";
     } else if ($weather_now == 51 || $weather_now == 53 || $weather_now == 55) {
         $picture = "drizzle";
-        $bg_image = "../backgrounds/rainy.jpg";
+        $bg_image = "../assets/backgrounds/rainy.jpg";
         $weather_decr = "Drizzle";
     } else if ($weather_now == 56 || $weather_now == 57) {
         $picture = "freezing_rain";
-        $bg_image = "../backgrounds/rainy.jpg";
+        $bg_image = "../assets/backgrounds/rainy.jpg";
         $weather_decr = "Freeing drizzle";
     } else if ($weather_now == 61 || $weather_now == 63 || $weather_now == 65) {
         $picture = "rain";
-        $bg_image = "../backgrounds/rainy.jpg";
+        $bg_image = "../assets/backgrounds/rainy.jpg";
         $weather_decr = "Rainy";
     } else if ($weather_now == 66 || $weather_now == 67) {
         $picture = "freezing_rain";
-        $bg_image = "../backgrounds/rainy.jpg";
+        $bg_image = "../assets/backgrounds/rainy.jpg";
         $weather_decr = "Freezing rain";
     } else if ($weather_now == 71 || $weather_now == 73 || $weather_now == 75) {
         $picture = "snow";
-        $bg_image = "../backgrounds/snowy.jpg";
+        $bg_image = "../assets/backgrounds/snowy.jpg";
         $weather_decr = "Snow fall";
     } else if ($weather_now == 77) {
         $picture = "snow_grains";
-        $bg_image = "../backgrounds/snowy.jpg";
+        $bg_image = "../assets/backgrounds/snowy.jpg";
         $weather_decr = "Snow grains";
     } else if ($weather_now == 80 || $weather_now == 81 || $weather_now == 82) {
         $picture = "rain";
-        $bg_image = "../backgrounds/rainy.jpg";
+        $bg_image = "../assets/backgrounds/rainy.jpg";
         $weather_decr = "Rain showers";
     } else if ($weather_now == 85 || $weather_now == 86) {
         $picture = "snow";
-        $bg_image = "../backgrounds/snowy.jpg";
+        $bg_image = "../assets/backgrounds/snowy.jpg";
         $weather_decr = "Snow showers";
     } else if ($weather_now == 95 || $weather_now == 96 || $weather_now == 99) {
         $picture = "thunderstorm";
-        $bg_image = "../backgrounds/rainy.jpg";
+        $bg_image = "../assets/backgrounds/rainy.jpg";
         $weather_decr = "Thunderstorm";
     }
     ?>
@@ -147,13 +182,45 @@
     echo " <div id='widget'>
         <div id='top-cover' style='background-image: url($bg_image); background-size: cover;'>";
 
-    // gets current date and month
-    $date_time = new DateTime($data["current_weather"]["time"]);
-    $month = $date_time->format('M');
-    $date = $date_time->format('d');
+    ?>
+    <?php
+    // gets system's date
+    date_default_timezone_set('America/New_York');
+    $month = date('M');
+    $date = date('d');
     echo "<div style='font-weight: bold; font-size: 15vw; text-align: left; padding: 3vw 2.5vw 0vw; margin-bottom: calc(-1 * 5vw); width: auto;' id='month'>$month</div>";
     echo "<div style='font-weight: bold; font-size: 16vw; text-align: left; padding-left: 2.5vw; width: auto;' id='month'>$date</div>";
-    echo "<div style='font-weight: bold; font-size: 19vw; text-align: left; padding-left: 2.5vw; width: auto; position: absolute; right: 2.5vw; top: 50vw;' ><span id='current_temp'>$current_temp</span>° F</div>";
+    echo "<div style='font-weight: bold; font-size: 19vw; text-align: left; padding-left: 2.5vw; width: auto; position: absolute; right: 2.5vw; top: 50vw;' id='current_temperature'><span>$current_temp</span>° F</div>";
+
+    // gets alerts from the weather bit api
+    $url_alerts = "https://api.weatherbit.io/v2.0/alerts?lat=41.0145&lon=-73.8726&key=3e9ad9f2717c45af874b3f794d75f65b";
+    $json_alerts = file_get_contents($url_alerts);
+    $data_alerts = json_decode($json_alerts, true);
+    $alert = $data_alerts["alerts"];
+    $warning = "";
+    $warning_count = 0;
+
+    // Checks if the API has any alerts, and then gets just the most urgent warnings
+    if (!empty($alert)) {
+        for ($i = 0; $i < count($alert); $i++) {
+            if ($alert[$i]["severity"] == "Warning") {
+                // Makes sure that duplicate warnings will not be displayed
+                if ($warning_count == 0) {
+                    $warning .= $warning . $alert[$i]["title"];
+                }
+                $warning_count++;
+            }
+        }
+    }
+    // Prints out urgent warnings if they were found
+    if ($warning != "") {
+        echo "<div id='alert' class='active'>
+                    <img src='../assets/weather_icons/bell.png' id='alert_icon' />
+                    <span>
+                    $warning
+                    </span>
+                </div>";
+    }
     ?>
     </div>
 
@@ -162,12 +229,12 @@
         $date_today = $data["daily"]["time"][0];
         $day_of_the_week_today = date("D", strtotime($date));
         echo "<div id='week_day_today' style='font-weight: bold; font-size: 8vw; text-align: center; padding: 2vw 0 4vw 0;'>$day_of_the_week_today</div>";
-        echo "<img src='../weather_icons/$picture.png' id='logo' style='width: 20vw; position: relative; left: 40vw;' />";
+        echo "<img src='../assets/weather_icons/$picture.png' id='logo' style='width: 20vw; position: relative; left: 40vw;' />";
         echo "<div id='weather_today' style='font-weight: 600; font-size: 5.5vw; text-align: center; padding: 2vw 0 4vw 0;'>$weather_decr</div>";
 
         echo "<div style='text-align: center; font-size: 3.5vw; padding-bottom: 2vw; font-weight: 300 '>High: <span>$highest_temp_today</span>°F; Low: <span>$lowest_temp_today</span>°F</div>
     <div style='text-align: center; font-size: 3.5vw; font-weight: 300; padding-bottom: 5vw;'><img
-            src='../weather_icons/Wind.png' class='small_icon' /> Wind: <span>$wind_today</span> mph</div>"
+            src='../assets/weather_icons/Wind.png' class='small_icon' /> Wind: <span>$wind_today</span> mph</div>"
             ?>
         <hr style='border-bottom: 0.05vw solid #545454;'>
 
@@ -214,14 +281,28 @@
 
             echo " <div class='forecast_prediction' id='forecast_$i'>
         <div class='weekday'>$day_of_the_week</div>
-        <img src='../weather_icons/$picture_forecast.png' class='weather_icon'/>
+        <img src='../assets/weather_icons/$picture_forecast.png' class='weather_icon'/>
         <div>↑<span >$highest_temp_forecast</span>F; ↓<span>$lowest_temp_forecast</span>F</div>
-        <div><img src='../weather_icons/Wind.png' class='wind_icon'/><span> $wind_forecast</span> mph</div>
+        <div><img src='../assets/weather_icons/Wind.png' class='wind_icon'/><span> $wind_forecast</span> mph</div>
     </div>";
         }
         ?>
     </div>
     </div>
 </body>
+<!-- adjusts location of the current temperature and the warning bubble based on the size of the warning text-->
+<script>
+    $(document).ready(function () {
+        // 3.5856573705179287vw -> a magic number -> min height for the alert
+        var alert_height = $('#alert').height();
+        var screen_width = $(window).width();
+        // alert height in vw
+        alert_height = alert_height / screen_width * 100;
+        var new_position = 65 - (alert_height - 3.5856573705179287) / 2;
+        $('#alert').css({ 'top': (new_position - alert_height / 2) + 'vw' });
+        window.console && console.log(alert_height);
+        $('#current_temperature').css({ 'top': 47 - alert_height + 'vw' });
+    });
+</script>
 
 </html>
